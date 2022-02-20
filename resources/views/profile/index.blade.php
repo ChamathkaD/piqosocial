2f37f11181b9f17807455997235025be2cede72d
Isuru Ahinsa <isurujayawickramat@gmail.com>
2022-02-20 18:14:47 +0530

    Created Some Components

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,109 +1,11 @@
 <x-app-layout>
 
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-        <div class="relative ">
-            <img src="{{ asset('img/cover.jpg') }}" alt="default img" class="rounded-b-lg h-96 w-full object-center" />
+        
+        <x-profile-header />
 
-            <div>
-                <button
-                    class="absolute bottom-5 right-10  inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-black bg-gray-50 hover:bg-gray-100 focus:outline-non">
-
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
-                    </svg>
-                    Edit Cover Photo
-                </button>
-            </div>
-
-        </div>
-
-        <div class="relative flex justify-between items-center">
-            <div class="flex">
-                <img src="https://i.pravatar.cc/150?img=31" alt="default img"
-                    class="h-15 w-15 rounded-full ml-10 -mt-10 ring-4 ring-white" />
-
-                <div class="ml-3 mt-3 space-y-1">
-                    <h2 class="text-3xl font-black">Chamathka Dissanayaka</h2>
-                    <p class="text-gray-500 font-semibold">1.8K Friends</p>
-                    <div class="flex -space-x-1 overflow-hidden">
-                        <img class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-                            src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt="">
-                        <img class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-                            src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt="">
-                        <img class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-                            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-                            alt="">
-                        <img class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt="">
-                        <img class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-                            src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt="">
-                        <img class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-                            src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt="">
-                        <img class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-                            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-                            alt="">
-                        <img class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt="">
-                    </div>
-                </div>
-            </div>
-
-            <button
-                class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm text-white font-semibold bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                </svg>
-                Edit Profile
-            </button>
-        </div>
-
-        <div class="mt-5">
-            <hr>
-
-            <div class="mt-5 hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
-                id="mobile-menu-4">
-                <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-base md:font-medium font-bold ">
-                    <li class="hover:bg-gray-200">
-                        <a href="#"
-                            class="m-2 block text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                            aria-current="page">Posts</a>
-                    </li>
-
-                    <li class="hover:bg-gray-200">
-                        <a href="#"
-                            class="m-2 block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-                    </li>
-
-                    <li class="hover:bg-gray-200">
-                        <a href="#"
-                            class="m-2 block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Friends</a>
-                    </li>
-
-                    <li class="hover:bg-gray-200">
-                        <a href="#"
-                            class="m-2 block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Photos</a>
-                    </li>
-
-                    <li class="hover:bg-gray-200">
-                        <a href="#"
-                            class="m-2 block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Videos</a>
-                    </li>
-
-                </ul>
-            </div>
+        <div class="my-3">
+            <x-profile-navbar />
         </div>
 
         <div class="grid grid-cols-12 gap-4">

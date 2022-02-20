3315c0901f05aad8c24e212feb0524bd84c7f884
ChamathkaD <chamathka.dissa@gmail.com>
2022-02-20 15:25:29 +0530

    profile.blade.php updated

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -70,6 +70,31 @@
 
        <div class="mt-5">
            <hr>
+
+           <div class="mt-5 hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-4">
+            <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-base md:font-medium font-bold ">
+              <li class="hover:bg-gray-200">
+                <a href="#" class="m-2 block text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page" >Posts</a>
+              </li>
+
+              <li class="hover:bg-gray-200">
+                <a href="#" class="m-2 block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+              </li>
+
+              <li class="hover:bg-gray-200">
+                <a href="#" class="m-2 block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Friends</a>
+              </li>
+
+              <li class="hover:bg-gray-200">
+                <a href="#" class="m-2 block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Photos</a>
+              </li>
+
+              <li class="hover:bg-gray-200">
+                <a href="#" class="m-2 block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Videos</a>
+              </li>
+              
+            </ul>
+          </div>
        </div>
 
         

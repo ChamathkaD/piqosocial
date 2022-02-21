36ed6e8bcf32da2f995494636431997248fa8666
ChamathkaD <chamathka.dissa@gmail.com>
2022-02-21 14:33:28 +0530

    navigation updated

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -33,13 +33,50 @@
                     </svg>
                 </div>
 
-                <div class="bg-gray-200 rounded-full p-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-black" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                    </svg>
-                </div>
+                <x-dropdown align="right" width="96">
+                    <x-slot name="trigger">
+
+                        <div class="bg-gray-200 rounded-full p-2 hover:cursor-pointer ">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5  text-black" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                            </svg>
+
+
+                        </div>
+                    </x-slot>
+
+                    <x-slot name="content">
+
+                        
+                            <x-dropdown-link>
+                                <div class="flex  items-center justify-between">
+                                    <h1 class="text-2xl font-bold mb-3 mr-0">Notifications</h1>
+                                    
+                                   
+                                   
+
+                                    
+                                    
+                                </div>
+                            </x-dropdown-link>
+
+                            <x-dropdown-link>
+                                <div class="flex items-center ">
+                                    <img src="https://i.pravatar.cc/150?img=31" alt="default img"
+                                        class="h-12 w-12 rounded-full" />
+                                    <div class="ml-2">
+                                        
+                                        <h3 class="font-bold">Isuru Dissanayaka</h3>
+                                        <p class="text-xs font-bold text-gray-400">Send you a friend request</p>
+                                    </div>
+                                </div>
+                                
+                            </x-dropdown-link>
+                        
+                    </x-slot>
+                </x-dropdown>
 
                 <x-dropdown align="right" width="96">
                     <x-slot name="trigger">
@@ -47,22 +84,17 @@
 
                         <button
                             class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name }}</div>
+
 
                             <div class="flex flex-row items-center space-x-4">
                                 <img src="https://i.pravatar.cc/150?img=31" alt="default img"
                                     class="h-8 w-8 rounded-full" />
                             </div>
 
-                            {{-- <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd"
-                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                            </div> --}}
+
                         </button>
+
+
                     </x-slot>
 
                     <x-slot name="content">
@@ -70,7 +102,8 @@
                         <div>
                             <x-dropdown-link>
                                 <div class="flex items-center ">
-                                    <img src="https://i.pravatar.cc/150?img=31" alt="default img" class="h-12 w-12 rounded-full" />
+                                    <img src="https://i.pravatar.cc/150?img=31" alt="default img"
+                                        class="h-12 w-12 rounded-full" />
                                     <div class="ml-2">
                                         <h3 class="font-bold">Chamayhka Dissanayaka</h3>
                                         <p class="text-xs font-bold text-gray-400">See Your Profile</p>
@@ -85,14 +118,18 @@
 
                             <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                                
-                               <div class="flex">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-10  " fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                                  </svg>
 
-                                {{ __('Log Out') }}
-                               </div>
+                                <div class="flex">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                        viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                                    </svg>
+
+                                    <span class="ml-2">
+                                        {{ __('Log Out') }}
+                                    </span>
+                                </div>
                             </x-dropdown-link>
                         </form>
                     </x-slot>

a97e2e28ee106533524c71b30d70397f4f6fc73c
Isuru Ahinsa <isurujayawickramat@gmail.com>
2022-02-22 16:04:29 +0530

    Dropdown Menu Updated

--- resources/views/partials/user-menu.blade.php
+++ resources/views/partials/user-menu.blade.php
@@ -4,7 +4,6 @@
         <button
             class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
 
-
             <div class="flex flex-row items-center space-x-4">
                 <img src="https://i.pravatar.cc/150?img=31" alt="default img" class="h-8 w-8 rounded-full" />
             </div>
@@ -13,17 +12,15 @@
 
     <x-slot name="content">
 
-        <div>
-            <x-dropdown-link>
-                <div class="flex items-center ">
-                    <img src="https://i.pravatar.cc/150?img=31" alt="default img" class="h-12 w-12 rounded-full" />
-                    <div class="ml-2">
-                        <h3 class="font-bold">Chamayhka Dissanayaka</h3>
-                        <p class="text-xs font-bold text-gray-400">See Your Profile</p>
-                    </div>
-                </div>
-            </x-dropdown-link>
+        <div class="flex items-center px-4 py-2">
+            <img src="https://i.pravatar.cc/150?img=31" alt="default img" class="h-12 w-12 rounded-full" />
+            <div class="ml-2 leading-none">
+                <h3 class="font-semibold">Chamayhka Dissanayaka</h3>
+                <a href="#" class="text-xs text-gray-400 hover:text-gray-500">See Your Profile</a>
+            </div>
         </div>
+
+        <hr>
 
         <!-- Authentication -->
         <form method="POST" action="{{ route('logout') }}">
@@ -33,13 +30,13 @@
                                 this.closest('form').submit();">
 
                 <div class="flex">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                     </svg>
 
-                    <span class="ml-2">
+                    <span class="ml-2 font-semibold">
                         {{ __('Log Out') }}
                     </span>
                 </div>
